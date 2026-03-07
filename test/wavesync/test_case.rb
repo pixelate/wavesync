1b8897eb85a793cd0823c1364c188662508957a7
Andreas Zecher <andreas@polylists.com>
2026-03-07 23:15:52 +0100

    Add audio fixtures and use in tests

--- test/wavesync/test_case.rb
+++ test/wavesync/test_case.rb
@@ -5,6 +5,7 @@ require 'mocha/minitest'
 
 module Wavesync
   class TestCase < Minitest::Test
+    FIXTURES_PATH = File.expand_path('../fixtures', __dir__).freeze
     def self.test(name, &)
       define_method("test_#{name.gsub(/\s+/, '_')}", &)
     end
